9617150c719ca6e4f67f7fb454590e149b563488
Riad Ali <alii.riad1234@gmail.com>
2025-02-06 15:30:02 +0100

    migrations created

---
+++ back-end/database/migrations/2025_02_06_110716_create_teachers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("user_id")->constrained("users")->onDelete("cascade");
+            $table->string("full_name");
+            $table->string("username")->unique()->nullable();
+            $table->string("phone")->unique()->nullable();
+            $table->string("address")->nullable();
+            $table->string("specialization")->nullable();
+            $table->string("picture")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teachers');
+    }
+};
---
+++ back-end/database/migrations/2025_02_06_112225_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("user_id")->constrained("users")->onDelete("cascade");
+            $table->foreignId("class_id")->constrained("classes")->onDelete("cascade");
+            $table->string("full_name");
+            $table->date("date_of_birth")->nullable();
+            $table->enum("gender",["female","male"]);
+            $table->string("address")->nullable();
+            $table->string("phone")->unique()->nullable();
+            $table->string("user_name")->unique()->nullable();
+            $table->string("picture")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
