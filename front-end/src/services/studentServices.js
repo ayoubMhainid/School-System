2e03c350d17fd55c90336a5cf660b1f020fce005
sofyan <soufianeboukir0@gmail.com>
2025-02-13 22:16:29 +0100

    get && search && filter(class,gender) students front-end

--- front-end/src/services/studentServices.js
+++ front-end/src/services/studentServices.js
@@ -6,6 +6,33 @@ export const getStudents = async (token,page) =>{
         headers : {
             Authorization : `Bearer ${token}`
         }
-    })
+    });
+    return response;
+}
+
+export const searchStudents = async (token,username) =>{
+    const response = await axios.get(`${apiUrl}/student/searchStudentsByUsername/${username}`,{
+        headers : {
+            Authorization : `Bearer ${token}`
+        }
+    });
+    return response;
+}
+
+export const getStudentsByClass = async (token,class_id) =>{
+    const response = await axios.get(`${apiUrl}/student/filterStudentsByClass/${class_id}`,{
+        headers : {
+            Authorization : `Bearer ${token}`
+        }
+    });
+    return response;
+} 
+
+export const filterStudentsByGender = async (token,gender) =>{
+    const response = await axios.get(`${apiUrl}/student/filterStudentsByGender/${gender}`,{
+        headers : {
+            Authorization : `Bearer ${token}`
+        }
+    });
     return response;
 }
