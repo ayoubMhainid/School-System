b09f33aff00ddba453334a83284d59e5606d1026
sofyan <soufianeboukir0@gmail.com>
2025-02-18 21:02:43 +0100

    merge conflict

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -6,7 +6,6 @@ import { Home } from "./shared/Home";
 import { Dashboard } from "./pages/admin/Dashboard";
 import { ManageStudents } from "./pages/admin/ManageStudents";
 import { ManageTeam } from "./pages/admin/ManageTeam";
-
 import { PersisLogin } from "./pages/auth/PersisLogin";
 import { ManageSubjects } from "./pages/admin/MannageSubjects";
 import { ManageTeacher } from "./pages/admin/ManageTeacher";
@@ -21,7 +20,7 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/admin/dashboard" element={<Dashboard />} />
           <Route path="/admin/students" element={<ManageStudents />} />
-          {/* <Route path="/admin/Subjects" element={<ManageSubjects />} /> */}
+          <Route path="/admin/Subjects" element={<ManageSubjects />} />
           <Route path="/admin/teachers" element={<ManageTeacher />} />
           <Route path="/admin/team" element={<ManageTeam />} />
           <Route path="/admin/subjects" element={<ManageSubjects />} />
