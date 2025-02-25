8889f659309d999675ae8cfc4fc2ff5c15f17e47
sofyan <soufianeboukir0@gmail.com>
2025-02-25 13:25:09 +0000

    front-end fix issues

--- front-end/src/pages/admin/ManageStudents.jsx
+++ front-end/src/pages/admin/ManageStudents.jsx
@@ -13,8 +13,7 @@ import { Table as TableSkeleton } from "../../components/Skeletons/Table";
 import { getClasses } from "../../services/classServices";
 import { errors } from "../../constants/Errors";
 import { useAppContext } from "../../context/AppContext";
-import { Add } from "../../components/modals/Add";
-
+import { Add } from "../../components/Modals/Add";
 export const ManageStudents = () => {
   const [students, setStudents] = useState([]);
   const [username, setUsername] = useState("");
